961391c0546b1507b56cbbb7f9a731e7cb222e53
palzkx2 <palzkx2@naver.com>
2024-11-08 15:38:00 +0900

    최종

--- src/main/marryus/src/body/Main.js
+++ src/main/marryus/src/body/Main.js
@@ -368,7 +368,6 @@ const Main = () => {
                         {sdmList.filter(name => name.category==='스튜디오').map((item, index) => (
                             <SwiperSlide key={index}>
                                 <div className='swiper-slide' style={{display:'flex', alignItems:'center', justifyContent:'center'}}>
-<<<<<<< HEAD
                                     <Link to={`/sdmArticle/${item.itemNm}`}>
                                         <div style={{textAlign: 'left'}}>
                                             <img src={`${API_SERVER_HOST}/api/sdm/view/${item.imageList[0].imgName}`} alt='' style={{position:'relative'}}/>
@@ -377,14 +376,6 @@ const Main = () => {
                                                 <p>{item.addr}</p>
                                                 <p style={{paddingBottom:'25px'}}>{item.price}</p>
                                             </div>
-=======
-                                    <div style={{textAlign: 'left'}}>
-                                        <img src={`${API_SERVER_HOST}/api/sdm/view/${item.imageList[0].imgName}`} alt='' style={{position:'relative'}}/>
-                                        <div className='imgdiv'>
-                                            <strong style={{display: 'block', fontSize: '18pt'}}>{item.itemNm}</strong>
-                                            <p>{item.addr}</p>
-                                            <p style={{paddingBottom:'25px'}}>{numeral(item.price).format('0,0')}</p>
->>>>>>> branch '카트와리뷰를_합쳤어용' of https://github.com/palzkx2/b_marryus.git
                                         </div>
                                     </Link>
                                 </div>
